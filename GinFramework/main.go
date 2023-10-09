20facc0dfe9c828721ba1bb4a4bbee471bb75e1d
thanitpong.p <tp.thanitpong@gmail.com>
2023-10-09 12:44:28 +0700

    no message

--- GinFramework/main.go
+++ GinFramework/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	
 )
 
 func main() {
@@ -10,8 +11,17 @@ func main() {
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello, Gin!",
+			"object": gin.H{"id": 1,
+				"title":              "iPhone 9",
+				"description":        "An apple mobile which is nothing like apple",
+				"price":              549,
+				"discountPercentage": 12.96,
+				"rating":             4.69,
+				"stock":              94},
 		})
 	})
 
 	router.Run(":8080")
 }
+
+
